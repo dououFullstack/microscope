9e26acca9882fc443b1df2a280adad3a5a450014
dououFullstack <13412133311@139.com>
2015-10-26 20:08:01 +0800

    Router sorry I still don't understand

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
-Meteor.subscribe('posts');
+// Meteor.subscribe('posts');
 
 //服务端pblication时加了参数,这里subscribe时就需要传参
 //这样的话，你就可以避免消耗大量的客户端内存，无论服务器端的总数据量有多大。
 //客户端能够具有可伸缩性：不去订阅全部数据，而是指选择你现在需要的数据去订阅。
-Meteor.subscribe('posts', 'doudou');
+// Meteor.subscribe('posts', 'doudou');
 
 // Autopublish : Publish all data to Client for Server
 
