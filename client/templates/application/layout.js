02605410324e8fbc8f53636540c21a6f07cef062
dououFullstack <13412133311@139.com>
2015-10-27 15:26:05 +0800

    Tracker.autorun

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -10,3 +10,14 @@ Template.layout.helpers({
 Tracker.autorun(function() {
   alert(Session.get('message'));
 });
+
+
+// Computations 代码块是根据响应式数据的变化去运行。
+// 如果你有一个响应式数据源（例如，一个 Session 变量）
+// 并且希望及时去响应它，你需要建立一个 Computations。
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    console.log('There are ' + Posts.find().count() + ' posts');
+  });
+//此函数 只要posts数量有变化就可主动输出console.log
+});
